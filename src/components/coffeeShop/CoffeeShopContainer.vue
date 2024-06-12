baeb729f93fea3fe4ef06e5047ad9b070e43ceeb
Maksim Lozovoy <maxlozovoy@gmail.com>
2024-06-12 21:43:11 +0300

    add Responsive

--- src/components/coffeeShop/CoffeeShopContainer.vue
+++ src/components/coffeeShop/CoffeeShopContainer.vue
@@ -1,8 +1,12 @@
 <template>
     <div id="coffee-shop">
-        <TitleComponent 
-            :title="coffeeShop.title" 
-            :p="coffeeShop.p"/>
+        <Responsive :breakpoints="{
+            tablet: el => el.width <= 768
+        }">
+            <TitleComponent slot-scope="el"
+                :title="coffeeShop.title"
+                :p="el.is.tablet ? coffeeShop.pTablet : coffeeShop.pDesktop"/>
+        </Responsive>
         <div class="coffee-shop__menu">
             <CoffeeShopDrinks/>
             <CoffeeShopDesserts/>
@@ -14,56 +18,41 @@
 import TitleComponent from '@/components/shared/TitleComponent.vue'
 import CoffeeShopDrinks from  './CoffeeShopDrinks.vue'
 import CoffeeShopDesserts from './CoffeeShopDesserts.vue'
+import { Responsive } from 'vue-responsive-components'
 
 export default {
     components: {
         TitleComponent,
         CoffeeShopDrinks,
-        CoffeeShopDesserts
+        CoffeeShopDesserts,
+        Responsive
     },
     data() {
         return {
             coffeeShop: {
                 title: 'Coffee shop',
-                p: ''
-            },
-            windowWidth: 0
+                pDesktop: `In our library, we have of cozy coffee shop, 
+                    welcoming in customers with frothy cappuccinos and 
+                    friendly conversation. 
+                    <br> 
+                    You can get a favorite book and read in coffee shop. 
+                    Our barista to cook you best coffee, 
+                    and also you can try desserts
+                    <br> 
+                    from bakery.`,
+                pTablet: `In our library, we have of cozy coffee shop, welcoming 
+                    in&nbsp;<br> 
+                    customers with frothy cappuccinos and friendly conversation. 
+                    <br> 
+                    You can get a favorite book and read in coffee shop. 
+                    Our barista 
+                    <br> 
+                    to cook you best coffee, and also you can try desserts from 
+                    <br> 
+                    bakery.`
+            }
         }
-    },
-    mounted() {
-        this.windowWidth = window.innerWidth
-        window.addEventListener('resize', this.handleResize)
-    },
-    beforeDestroy() {
-        window.removeEventListener('resize', this.handleResize)
-    },
-    methods: {
-        handleResize() {
-            this.windowWidth = window.innerWidth;
-        }
-    },
-    watch: {
-    windowWidth(newWidth) {
-      if (newWidth <= 768) {
-        this.coffeeShop.p = `In our library, we have of cozy coffee shop, welcoming 
-            in&nbsp;<br>
-            customers with frothy cappuccinos and friendly conversation. 
-            <br>
-            You can get a favorite book and read in coffee shop. 
-            Our barista 
-            <br> 
-            to cook you best coffee, and also you can try desserts from 
-            <br>
-            bakery.`
-      } else {
-        this.coffeeShop.p = `In our library, we have of cozy coffee shop, 
-            welcoming in customers with frothy cappuccinos and friendly conversation. 
-            <br>
-            You can get a favorite book and read in coffee shop. Our barista to 
-            cook you best coffee, and also you can try desserts from bakery.`
-      }
     }
-  }
 }
 </script>
 
