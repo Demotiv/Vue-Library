b84b19d98d8b863371a71dab3adc795ecac60db9
Maksim Lozovoy <maxlozovoy@gmail.com>
2024-06-14 13:34:10 +0300

    Animation hamburger lines

--- src/components/header/header-tablet/HeaderHamburger.vue
+++ src/components/header/header-tablet/HeaderHamburger.vue
@@ -1,13 +1,25 @@
 <template>
-    <div class="hamburger">
-        <div></div>
-        <div></div>
-        <div></div>
+    <div 
+        class="hamburger"
+        @click="toggleMenu">
+        <span class="hamburger__line" :class="{ 'active': onMenu }"></span>
+        <span class="hamburger__line" :class="{ 'active': onMenu }"></span>
+        <span class="hamburger__line" :class="{ 'active': onMenu }"></span>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return{
+            onMenu: false
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.onMenu = !this.onMenu
+        }
+    }
 }
 </script>
 
@@ -21,10 +33,23 @@ export default {
         gap: 10px;
         cursor: pointer;
 
-        & div {
+        &__line {
             border: 1px solid $white;
             width: 45px;
             height: 0px;
+            transition: transform 0.3s ease, opacity 0.3s ease;
+
+            &.active:nth-child(1) {
+                transform: translateY(12px) rotateZ(45deg);
+            }
+
+            &.active:nth-child(2) {
+                opacity: 0;
+            }
+
+            &.active:nth-child(3) {
+                transform: translateY(-12px) rotateZ(-45deg);
+            }
         }
     }
 }
