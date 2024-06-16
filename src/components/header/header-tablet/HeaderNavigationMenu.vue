086ec57516ba4455deef0a7360d4434237f4e97b
Maksim Lozovoy <maxlozovoy@gmail.com>
2024-06-16 12:31:20 +0300

    repair

--- src/components/header/header-tablet/HeaderNavigationMenu.vue
+++ src/components/header/header-tablet/HeaderNavigationMenu.vue
@@ -1,8 +1,6 @@
 <template>
     <transition name="slide">
-        <div 
-            class="hamburger-navigation" 
-            v-if="onMenu">
+        <div class="hamburger-navigation" v-if="onMenu">
             <NavigationBlock/>
         </div>
     </transition>
